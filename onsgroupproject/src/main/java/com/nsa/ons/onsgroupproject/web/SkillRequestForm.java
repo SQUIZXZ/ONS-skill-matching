77930d17c65874072770c4c4d954e65767b40d85
Ewan Stacey <StaceyE1@cardiff.ac.uk>
2019-11-15 14:50:11 +0000

    Skill request form arguments created

--- onsgroupproject/src/main/java/com/nsa/ons/onsgroupproject/web/SkillRequestForm.java
+++ onsgroupproject/src/main/java/com/nsa/ons/onsgroupproject/web/SkillRequestForm.java
@@ -1,4 +1,35 @@
 package com.nsa.ons.onsgroupproject.web;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SkillRequestForm {
+
+    @NotNull
+    @Size(min = 2, max = 100, message = "Invalid Name")
+    private String firstName;
+
+    @NotNull
+    @Size(min = 2, max = 100, message = "Invalid Surname")
+    private String surname;
+
+    @NotNull
+    @Size(min = 2, max = 100, message = "Invalid department name")
+    private String department;
+
+    @NotNull
+    @Size(min = 2, max = 100, message = "Skill not Found") //This will likely be a dropdown when we get the skill database up and running
+    private String skill;
+
+    @NotNull
+    @Size(min = 2, max = 300, message = "Your Description is too long")
+    private String taskDescription;
+
 }
