d14c9f6fe77be90f9b7b0b1d70cec739c468ba58
Daniel Fulop <FulopD@cardiff.ac.uk>
2019-11-15 14:47:12 +0000

    Added path variable for skill request page

--- onsgroupproject/src/main/java/com/nsa/ons/onsgroupproject/web/SkillRequestController.java
+++ onsgroupproject/src/main/java/com/nsa/ons/onsgroupproject/web/SkillRequestController.java
@@ -1,15 +1,17 @@
 package com.nsa.ons.onsgroupproject.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SkillRequestController {
 
-    @RequestMapping(path = "skillRequest", method = RequestMethod.GET)
-    public String skillRequest() {
-
+    @RequestMapping(path = "skillRequest/{furl}", method = RequestMethod.GET)
+    public String skillRequest(@PathVariable("furl") String furl, Model model) {
+        model.addAttribute("furl", furl);
         return "RequestPage";
     }
 
