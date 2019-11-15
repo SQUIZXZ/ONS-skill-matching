719b7a7c397100410efcca3a4aace4a94eee8385
Ewan Stacey <StaceyE1@cardiff.ac.uk>
2019-11-15 14:04:33 +0000

    Added spring project

---
+++ onsgroupproject/src/main/java/com/nsa/ons/onsgroupproject/OnsgroupprojectApplication.java
@@ -0,0 +1,13 @@
+package com.nsa.ons.onsgroupproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnsgroupprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnsgroupprojectApplication.class, args);
+    }
+
+}
---
+++ onsgroupproject/src/test/java/com/nsa/ons/onsgroupproject/OnsgroupprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsa.ons.onsgroupproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnsgroupprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
