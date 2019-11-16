b99b45022bd1a9ecc2ba9fd6d60702acb1188b10
unknown <fulopd@cardiff.ac.uk>
2019-11-16 16:07:47 +0000

    Added data.sql file with dummy data for testing

---
+++ onsgroupproject/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into skillRequests (id, first_name, surname_name, furl, department, skill, `desc`) values (null, 'Daniel', 'Fulop', 'python-help', 'operations', 'Python', 'Need help with python pls');
