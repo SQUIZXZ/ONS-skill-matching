eb6a8e26943c04c6df14bf421255c96a15055e40
Ewan Stacey <StaceyE1@cardiff.ac.uk>
2019-11-18 12:35:37 +0000

    tests for the  request page complete also fixed the data which may have been broken by me on accident, oops

--- onsgroupproject/src/main/resources/data.sql
+++ onsgroupproject/src/main/resources/data.sql
@@ -1,1 +1,1 @@
-x`insert into skill_requests (id, first_name, last_name, furl, department, skill, info) values (null, 'Daniel', 'Fulop', 'python-help', 'operations', 'Python', 'Need help with python');
+insert into skill_requests (id, first_name, last_name, furl, department, skill, info) values (null, 'Daniel', 'Fulop', 'python-help', 'operations', 'Python', 'Need help with python');
