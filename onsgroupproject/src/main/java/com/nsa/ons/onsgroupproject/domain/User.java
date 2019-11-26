c2f92464da1b8ce142abaef13f7ee6f9c8c1e1df
c1843476 <wallisf@cardiff.ac.uk>
2019-11-26 19:41:04 +0000

    Implemented WebSecurityConfig and User domain along with dependencies

---
+++ onsgroupproject/src/main/java/com/nsa/ons/onsgroupproject/domain/User.java
@@ -0,0 +1,30 @@
+package com.nsa.ons.onsgroupproject.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Transient
+    @Column(name = "passwordConfirm")
+    private String passwordConfirm;
+
+}
